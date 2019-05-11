2feb12ea49ba91a9490f86cb69b74d25e9c18154
NisLox <willburgess95@gmail.com>
2019-05-11 16:12:16 +0100

    Pushing more

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ButtonDropdown, DropdownMenu, DropdownItem, DropdownToggle } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.select = this.select.bind(this);
+    this.useInformation = this.useInformation.bind(this);
+    this.state = {
+      dropdownOpen: false,
+      value : "Home"
+    };
+  }
+
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+  select(event) {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+      value: event.target.innerText
+    });
+  }
+
+  useInformation(){
+    console.log(this.state.value)
+  }
+
+  render() {
+    return (
+      <div className="App">
+       <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <DropdownToggle>{this.state.value}</DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem onClick={this.select}>Work</DropdownItem>
+          <DropdownItem onClick={this.select}>Contact</DropdownItem>
+        </DropdownMenu>
+      </ButtonDropdown>
+
+      <button type="button" onClick={this.useInformation} class="btn btn-primary">Primary</button>
+      </div>
+    );
+  }
 }
 
 export default App;
